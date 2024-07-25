63a97d60e6ab5ec6f71cf71ed32f11ea96a665a1
Your Name <you@example.com>
2024-07-25 15:25:55 +0530

    remove cors

--- server.js
+++ server.js
@@ -9,12 +9,6 @@ const url = 'mongodb://127.0.0.1:27017';
 const databaseName = 'Ecommerce';
 const client = new MongoClient(url);
 
-const cors = require('cors');
-
-app.use(cors({
-    origin: 'https://bo-server-side-1.onrender.com'
-  }));
-
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname +"/public")))
