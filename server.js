7000f333becd97d33c6ff3131f04dbdcc91e0102
Your Name <you@example.com>
2024-07-26 12:56:31 +0530

    curly brackets comment

--- server.js
+++ server.js
@@ -61,11 +61,13 @@ app.post('/downloadData', async (req, res) => {
         // console.log('document.plots =', document.plots);
         
         console.log(`Data saved to ${basePath}`);
-        res.status(200).send({
+        res.status(200).send(
+            // {
             // plots: document.plots,
             // csv: document.csv || null
             "Success from node server"
-        });
+        // }
+    );
     } catch (error) {
         console.error('Error:', error);
         res.status(500).send('Error fetching data');
